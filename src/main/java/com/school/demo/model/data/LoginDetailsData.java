6aac295d4c541ad07c7599e84a658d634b7d479f
m3hdibk <mehd.bk.i@gmail.com>
2020-03-15 23:06:09 +0100

    add Data POJO
    Signed-off-by: m3hdibk <mehd.bk.i@gmail.com>

--- src/main/java/com/school/demo/model/data/LoginDetailsData.java
+++ src/main/java/com/school/demo/model/data/LoginDetailsData.java
@@ -4,5 +4,83 @@
 
 package com.school.demo.model.data;
 
+import com.school.demo.util.Constants;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class LoginDetailsData {
+
+
+    private Long id;
+
+    private String username;
+
+    private String email;
+
+    private String password;
+
+    private Set<Constants.ERole> roles = new HashSet<>();
+
+    public LoginDetailsData() {
+    }
+
+    public LoginDetailsData(Long id, String username, String email, String password, Set<Constants.ERole> roles) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.roles = roles;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Constants.ERole> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Constants.ERole> roles) {
+        this.roles = roles;
+    }
+
+    @Override
+    public String toString() {
+        return "LoginDetailsData{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", roles=" + roles +
+                '}';
+    }
 }
